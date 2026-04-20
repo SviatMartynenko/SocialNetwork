24ac1d341702d6ab6b2db1a989e76a76e36d0b51
rainofpain <guestnewbie013@gmail.com>
2026-04-20 19:16:03 +0300

    minor improvements

--- social_network/static/js/active.js
+++ social_network/static/js/active.js
@@ -1,6 +1,6 @@
 navItems = document.querySelectorAll(".nav-item")
 navItems.forEach(item => {
     if (item.href == window.location.href) {
-        item.classList.add('active');
+        item.classList.add('active-nav-item');
     }
 });
