59a8e68d65bd64a7d0c7b99dc6122cf71ebf3215
Sofiia <sonatokarcuk@gmail.com>
2026-04-21 21:09:12 +0300

    styles for registration and authorization are set

--- social_network/user_app/forms.py
+++ social_network/user_app/forms.py
@@ -5,20 +5,35 @@ from .models import User
 class RegisterForm(UserCreationForm):
     password1 = forms.CharField(
             label="Пароль",
-            widget = forms.PasswordInput(attrs = {'class': 'form-field'})
+            widget = forms.PasswordInput(attrs = {
+                    'class': 'form-field',
+                    'placeholder': 'Введи пароль'
+                })
         )
     password2 = forms.CharField(
             label="Підтвердити пароль",
-            widget = forms.PasswordInput(attrs = {'class': 'form-field'})
+            widget = forms.PasswordInput(attrs = {
+                'class': 'form-field',
+                'placeholder': 'Повтори пароль'
+                })
         )
     class Meta:
         model = User
         fields = ['email','password1', 'password2']
         widgets = {
-            'email': forms.EmailInput(attrs = {'class': 'form-field'})
+            'email': forms.EmailInput(attrs = {
+                'class': 'form-field',
+                'placeholder': 'you@example.com'
+            })
         }
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget = forms.TextInput(attrs = {'class': 'form-field'}))
-    password = forms.CharField(widget = forms.PasswordInput(attrs = {'class': 'form-field'}))
+    username = forms.CharField(widget = forms.TextInput(attrs = {
+        'class': 'form-field',
+        'placeholder': 'you@example.com'
+        }))
+    password = forms.CharField(widget = forms.PasswordInput(attrs = {
+        'class': 'form-field',
+        'placeholder': 'Введи пароль'
+        }))
         
