d21a4968635d8f29aea8d90a729041291050d77d
rainofpain <guestnewbie013@gmail.com>
2026-04-21 21:51:07 +0300

    created auth form switch script

--- social_network/user_app/static/user_app/js/auth.js
+++ social_network/user_app/static/user_app/js/auth.js
@@ -1,8 +1,23 @@
 function getCSRFtoken(){
-    return document.querySelector("meta[name='csrf-token']").getAttribute('content')
+    return document.querySelector("meta[name='csrf-token']").getAttribute('content');
 }
-console.log(getCSRFtoken())
+console.log(getCSRFtoken());
 
 const navigation = document.querySelector(".navigation");
 navigation.style.justifyContent = "center";
 
+const formRegister = document.querySelector(".register-div");
+const formLogin = document.querySelector(".login-div");
+
+const loginTitle = formRegister.querySelector(".register-login-text")
+const registerTitle = formLogin.querySelector(".registration-text")
+
+loginTitle.addEventListener("click", () => {
+    formRegister.style.display = "none";
+    formLogin.style.display = "flex";
+});
+
+registerTitle.addEventListener("click", () => {
+    formLogin.style.display = "none";
+    formRegister.style.display = "flex";
+});
